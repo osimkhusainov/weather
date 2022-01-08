d81213bc5642e5f9ad7abf9e4f5dc334cca8a9e3
Osim Khusainov <keedice@gmail.com>
2022-01-08 20:02:59 +0300

    a

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ hbs.registerPartials(partialsPath);
 // setup static directory to serve
 app.use(express.static(publicDirectory));
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index", {
     title: "Weather",
     name: "Osim Khusainov",
